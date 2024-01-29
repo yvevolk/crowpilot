48241305f8594559bb4c0fac3b258a47f3ea7f82
Yve Volkovaya <yvevolkovaya@gmail.com>
2024-01-29 17:25:09 +0000

    prepare for FE development

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
 const app = require('./app.js');
-const { PORT = 9090 } = process.env.PORT || 9090;
+const port = process.env.PORT || 9090;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+app.listen(port, () => console.log(`Listening on ${PORT}...`));
